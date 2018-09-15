99324a91d7d3dc3cb79621f723adb25e96e22f1b
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-09-15 14:07:45 +0200

    extract class

---
+++ src/main/java/katas/SimplePokerHands.java
@@ -0,0 +1,31 @@
+package katas;
+
+import java.util.Arrays;
+
+public class SimplePokerHands {
+	
+	public String findHand(String cardsString) {
+		String[] sortedCards = cardsString.split(" ");
+		String pair = findHighestPairInCards(sortedCards);
+		if (pair == null) {
+			Arrays.sort(sortedCards, new CardComparator());
+			return sortedCards[sortedCards.length - 1];
+		} else {
+			return pair;
+		}
+	}
+	
+	private String findHighestPairInCards(String[] sortedCards) {
+		String card1 = sortedCards[0];
+		String card2 = sortedCards[1];
+		if (CardComparator.compareValues(card1, card2) == 0) {
+			return makeHandOfCards(card1, card2);
+		} else {
+			return null;
+		}
+	}
+	
+	private String makeHandOfCards(String card1, String card2) {
+		return card1 + " " + card2;
+	}
+}
--- src/test/java/katas/TestSimplePokerHands.java
+++ src/test/java/katas/TestSimplePokerHands.java
@@ -33,29 +33,8 @@ public class TestSimplePokerHands {
 	public void findPair() {
 		assertEquals("K5 C5", findHand("K5 C5"));
 	}
-	
-	public String findHand(String cardsString) {
-		String[] sortedCards = cardsString.split(" ");
-		String pair = findHighestPairInCards(sortedCards);
-		if (pair == null) {
-			Arrays.sort(sortedCards, new CardComparator());
-			return sortedCards[sortedCards.length - 1];
-		} else {
-			return pair;
-		}
-	}
-	
-	private String findHighestPairInCards(String[] sortedCards) {
-		String card1 = sortedCards[0];
-		String card2 = sortedCards[1];
-		if (CardComparator.compareValues(card1, card2) == 0) {
-			return makeHandOfCards(card1, card2);
-		} else {
-			return null;
-		}
-	}
-	
-	private String makeHandOfCards(String card1, String card2) {
-		return card1 + " " + card2;
+
+	private String findHand(String cardsString) {
+		return new SimplePokerHands().findHand(cardsString);
 	}
 }
