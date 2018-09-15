c0278f95c458418b21614d6411ed28df4c6b3098
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-09-15 11:21:59 +0200

    add unused code

--- src/test/java/katas/TestSimplePokerHands.java
+++ src/test/java/katas/TestSimplePokerHands.java
@@ -3,6 +3,7 @@ package katas;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 import org.junit.Test;
 
@@ -21,6 +22,21 @@ public class TestSimplePokerHands {
 	public String findHand(String cards) {
 		String[] splittedCards = cards.split(" ");
 		Arrays.sort(splittedCards);
+		Comparator<String> cardComparator = new Comparator<String>() {
+
+			public int compare(String o1, String o2) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+		};
+		
 		return splittedCards[splittedCards.length - 1];
 	}
+	
+	public String getValue(String card) {
+		return card.substring(1);
+	}
+	
+	
 }
