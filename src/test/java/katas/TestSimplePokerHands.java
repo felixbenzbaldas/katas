39d0c266ebffee4b07450d36f12559b2ac0932fc
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-09-15 11:08:40 +0200

    give hand back

--- src/test/java/katas/TestSimplePokerHands.java
+++ src/test/java/katas/TestSimplePokerHands.java
@@ -1,5 +1,17 @@
 package katas;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class TestSimplePokerHands {
 
+	@Test
+	public void giveHand() {
+		assertEquals("K5", findHand("K5"));
+	}
+	
+	public String findHand(String cards) {
+		return cards;
+	}
 }
