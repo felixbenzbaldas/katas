984f65ca6418b000067e51b09c9ce526ea40e15f
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-09-15 14:53:41 +0200

    add equals

--- src/main/java/katas/Hand.java
+++ src/main/java/katas/Hand.java
@@ -11,5 +11,30 @@ public class Hand {
 		cards.add(card1);
 		cards.add(card2);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cards == null) ? 0 : cards.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hand other = (Hand) obj;
+		if (cards == null) {
+			if (other.cards != null)
+				return false;
+		} else if (!cards.equals(other.cards))
+			return false;
+		return true;
+	}
 	
 }
