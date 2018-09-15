ef63efde8729597e0c22b6e7c6b67fa6f0b67a73
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-09-15 14:23:06 +0200

    remove flaw and simplify tests

--- src/test/java/katas/TestSimplePokerHands.java
+++ src/test/java/katas/TestSimplePokerHands.java
@@ -8,22 +8,22 @@ public class TestSimplePokerHands {
 
 	@Test
 	public void findHandUnortedCards() {
-		assertEquals("K7", findHand("K4 K7 K6"));
+		testExpectedActual("K7", "K4 K7 K6");
 	}
 	
 	@Test
 	public void findHand_images() {
-		assertEquals("KD", findHand("K4 KD K6"));
+		testExpectedActual("KD", "K4 KD K6");
 	}
 	
 	@Test
 	public void findHand_multiColors() {
-		assertEquals("C8", findHand("C8 K2"));
+		testExpectedActual("C8", "C8 K2");
 	}
 
 	@Test
 	public void findHand_ten() {
-		assertEquals("K10", findHand("K4 K10 K6"));
+		testExpectedActual("K10", "K4 K10 K6");
 	}
 	
 	
@@ -33,7 +33,7 @@ public class TestSimplePokerHands {
 	}
 
 	private void testExpectedActual(String expected, String actual) {
-		assertEquals(expected, actual);
+		assertEquals(expected, findHand(actual));
 	}
 
 	private String findHand(String cardsString) {
