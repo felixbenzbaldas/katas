91e3a9e59953100e663e60a152d308531b94ef17
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-09-29 20:39:01 +0200

    make Cards comparable

--- src/main/java/katas/Card.java
+++ src/main/java/katas/Card.java
@@ -1,6 +1,6 @@
 package katas;
 
-public class Card {
+public class Card implements Comparable<Card> {
 	String cardString;
 	
 	public String getValue() {
@@ -10,5 +10,9 @@ public class Card {
 	public Card(String cardString) {
 		super();
 		this.cardString = cardString;
+	}
+
+	public int compareTo(Card o) {
+		return new CardComparator().compare(this.cardString, o.cardString);
 	}
 }
