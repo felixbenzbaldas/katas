cf0ca554c2a9cb57bc754703887c1fbd7578efa5
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-09-29 20:30:08 +0200

    improve semantic

--- src/main/java/katas/SimplePokerHands.java
+++ src/main/java/katas/SimplePokerHands.java
@@ -24,14 +24,14 @@ public class SimplePokerHands {
 			Card card1 = new Card(sortedCards[i - 1]);
 			Card card2 = new Card(sortedCards[i]);
 			if (card1.getValue().equals(card2.getValue())) {
-				return toCards(card1.cardString, card2.cardString);
+				return toCards(card1, card2);
 			}
 		}
 		return null;
 	}
 	
 	
-	private String toCards(String card1, String card2) {
-		return card1 + " " + card2;
+	private String toCards(Card card1, Card card2) {
+		return card1.cardString + " " + card2.cardString;
 	}
 }
