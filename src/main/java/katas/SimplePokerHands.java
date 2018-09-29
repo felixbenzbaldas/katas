7d8f4774bb6335a04fa975310a7f428f21d0218e
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-09-29 20:41:32 +0200

    rename

--- src/main/java/katas/SimplePokerHands.java
+++ src/main/java/katas/SimplePokerHands.java
@@ -24,14 +24,18 @@ public class SimplePokerHands {
 			Card card1 = new Card(sortedCards[i - 1]);
 			Card card2 = new Card(sortedCards[i]);
 			if (card1.getValue().equals(card2.getValue())) {
-				return toCards(card1, card2);
+				return toCardsString(card1, card2);
 			}
 		}
 		return null;
 	}
 	
 	
-	private String toCards(Card card1, Card card2) {
+	private String toCardsString(Card card1, Card card2) {
 		return card1.cardString + " " + card2.cardString;
 	}
+	
+	
+	
+	
 }
