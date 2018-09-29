c75b32aca4dff57617e7fbb2699c6ab6e93a623d
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-09-29 20:45:19 +0200

    improve semantic

--- src/main/java/katas/SimplePokerHands.java
+++ src/main/java/katas/SimplePokerHands.java
@@ -3,7 +3,7 @@ package katas;
 import java.util.Arrays;
 
 public class SimplePokerHands {
-	
+
 	public String findHand(String cardsString) {
 		String[] sortedCards = sortCards(cardsString.split(" "));
 		String pair = findHighestPairInSortedCards(sortedCards);
@@ -13,12 +13,12 @@ public class SimplePokerHands {
 			return pair;
 		}
 	}
-	
+
 	private String[] sortCards(String[] cards) {
 		Arrays.sort(cards, new CardComparator());
 		return cards;
 	}
-	
+
 	private String findHighestPairInSortedCards(String[] sortedCards) {
 		for (int i = sortedCards.length - 1; i > 0; i--) {
 			Card card1 = new Card(sortedCards[i - 1]);
@@ -29,13 +29,14 @@ public class SimplePokerHands {
 		}
 		return null;
 	}
-	
-	
+
 	private String toCardsString(Card card1, Card card2) {
-		return card1.cardString + " " + card2.cardString;
+		String toReturn = card1.cardString;
+		for (int i = 0; i < 1; i++) {
+			toReturn += " " + card2.cardString;
+
+		}
+		return toReturn;
 	}
-	
-	
-	
-	
+
 }
