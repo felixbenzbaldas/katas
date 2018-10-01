0d8a82f6bd6c3e4683edc32802fe01df89d8965c
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-10-01 16:26:40 +0200

    minor change

--- src/main/java/katas/Card.java
+++ src/main/java/katas/Card.java
@@ -1,16 +1,18 @@
 package katas;
 
 public class Card implements Comparable<Card> {
+	
 	private String cardString;
+	
+	public Card(String cardString) {
+		super();
+		this.cardString = cardString;
+	}
 	
 	public String getValue() {
 		return cardString.substring(1);
 	}
 
-	public Card(String cardString) {
-		super();
-		this.cardString = cardString;
-	}
 
 	public int compareTo(Card o) {
 		return new CardValueComparator().compare(this, o);
